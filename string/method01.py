445eea7a461eb726b3caf537671e83adb478e5a3
jmills358 <john.mills1@oneverizon.com>
2019-08-22 14:21:28 +0000

    functions

---
+++ string/method01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def main():
+     """ Run-time code"""
+     lilstring = "Alta3 Research offers classes on python coding"
+     newlist = lilstring.split(" ")
+     print(newlist)
+     myiplist = ["192", "168", "0", "12"]
+     singleip = ".".join(myiplist)
+     print(singleip)
+main()
