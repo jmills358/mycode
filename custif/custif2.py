f250f190cbeed082c70438fae17c76356b28e911
jmills358 <john.mills1@oneverizon.com>
2019-08-23 14:21:35 +0000

    protocol

--- custif/custif2.py
+++ custif/custif2.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 while(True):
-    message = 'Please enter a routing protocol'
-    print('what routing Protocol are you using ?')
+    message = 'Please enter a routing protocol rip ,ospf, eigrp, bgp or q to quit'
+   # print('what routing Protocol are you using ?')
+    print(message)
+    
     connection = (input())
     if connection == "rip":
-        message2 =  'The Hop count for RIP is 15.'
+        print('The Hop count for RIP is 15.')
     elif connection == "ospf":
-        message2 =  'the AD for OSPF is 110.'
+        print(  'the AD for OSPF is 110.')
     elif connection == "EIGRP":
-        message2 =  'the maxe AD for EIGRP is 255.'
+        print('the maxe AD for EIGRP is 255.')
     elif connection == "BGP":
-        message2 =  'BGP AD uses Path Vector.'
+        print( 'BGP AD uses Path Vector.')
     elif connection == "q":
         break
 
     else:
-        message2 =  'Please enter  a routing protocol.'
-    print(message2)
+       print (message)# =  'Please enter  a routing protocol.'
+   
 
